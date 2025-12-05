c07010659e9a5e7ab2129dcee0d7235c4ed44ed1
wushengkai <wushengkai@cvte.com>
2025-12-05 10:13:54 +0800

    add concerto

--- GEN_policy/install.sh
+++ GEN_policy/install.sh
@@ -2,15 +2,22 @@ conda create -n GEN python=3.11
 conda activate GEN
 
 # Move to the ROOT of this repo
-pip install torch==2.8.0 torchvision==0.23.0 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+#pip install torch==2.9.0 torchvision==0.24.0 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install torch==2.3.1 torchvision==0.18.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # Install sonata
 cd GEN/utils/sonata
 pip install spconv-cu124 -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install torch-scatter -f https://data.pyg.org/whl/torch-2.8.0+cu125.html
-pip install flash-attn==2.8.3 --no-build-isolation
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.3.1+cu121.html
+#pip install flash-attn==2.8.3 --no-build-isolation
+MAX_JOBS=4 pip install flash-attn --no-build-isolation
 pip install huggingface_hub==0.23.2 -i https://pypi.tuna.tsinghua.edu.cn/simple
+python setup.py develop
+cd ../../..
+
+# Install Concerto
+cd GEN/utils/Concerto
 python setup.py develop
 cd ../../..
 
